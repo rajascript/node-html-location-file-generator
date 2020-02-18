1fd5fd889f65316a7af993b6c462138a546713e9
Raja Raghav <raja_raghav@hotmail.com>
2020-02-18 20:37:51 +0530

    express server on 3000, creating location html files using /save_location_html_file api

---
+++ index.js
@@ -0,0 +1,37 @@
+const app = require("express")();
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const createHtmlTemplate = function createHtmlTemplate(validLocation) {
+  //html template to be saved inside file
+  return `<html>
+        <body>
+        <div>
+        ${validLocation}
+        </div>
+        </body>
+    </html>`;
+};
+const isLocationValid = function isLocationVaid(location) {
+  if (location && typeof location === "string") {
+    return true;
+  } else return false;
+};
+
+app.use(bodyParser.json());
+
+app.post("/api/save_location_html_file", (req, res) => {
+  console.log(req.body);
+  const { location } = req.body;
+  if (isLocationValid(location)) {
+    let htmlTemplate = createHtmlTemplate(location);
+    fs.writeFile(`${Date.now()}_${location}.html`, htmlTemplate, err => {
+      if (err) console.log(err);
+    });
+  } else {
+    res
+      .status(403)
+      .json({ errorMessage: "location is a required 'String' field" });
+  }
+});
+
+app.listen(3000);

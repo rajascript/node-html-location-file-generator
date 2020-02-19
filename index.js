8582b29bfae530ebb1deb95f6f2a7b14121d1198
Raja Raghav <raja_raghav@hotmail.com>
2020-02-19 13:09:09 +0530

    added cors support

--- index.js
+++ index.js
@@ -16,10 +16,27 @@ const isLocationValid = function isLocationVaid(location) {
     return true;
   } else return false;
 };
+//cors setup
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Authorization, Origin, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+//cors setup
 app.use(bodyParser.json());
 
 app.post("/api/save_location_html_file", (req, res) => {
+  console.log(req.body);
   const { location } = req.body;
   if (isLocationValid(location)) {
     let htmlTemplate = createHtmlTemplate(location);
